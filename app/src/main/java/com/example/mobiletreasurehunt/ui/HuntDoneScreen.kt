c7388b0eb299b1d04c9a6b1458820ac22f4a79c3
canfieca <canfieca@oregonstate.edu>
2025-03-13 16:09:46 -0700

    Added Home button to the huntDoneScreen

--- app/src/main/java/com/example/mobiletreasurehunt/ui/HuntDoneScreen.kt
+++ app/src/main/java/com/example/mobiletreasurehunt/ui/HuntDoneScreen.kt
@@ -5,17 +5,22 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import java.net.URLDecoder
 import java.nio.charset.StandardCharsets
 
 @Composable
 fun HuntDoneScreen(
+    navController: NavController,
     clueInfo: String,
     elapsedTime: Long,
     modifier: Modifier = Modifier.fillMaxSize()
@@ -34,6 +39,12 @@ fun HuntDoneScreen(
             Text("Congratulations you completed the Mobile Treasure Hunt!", fontSize = 24.sp)
             Text("Last Clue Location: $decodedClueInfo", fontSize = 18.sp)
             Text("Total Time: ${elapsedTime}s", fontSize = 18.sp)
+            Button(
+                onClick = { navController.navigate("startHuntScreen") },
+                modifier = Modifier.padding(top = 16.dp)
+            ) {
+                Text("Home")
+            }
         }
     }
 }
